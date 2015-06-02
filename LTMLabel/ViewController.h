58049ead45eba4e1e8e1d44a29e32cf57a7bdcd6
Sebastian <bastianschilbe@users.noreply.github.com>
2015-06-02 23:50:27 +0200

    Initial Commit

---
+++ LTMLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTMLabel
+//
+//  Created by Sebastian Schilbe on 02.06.15.
+//  Copyright (c) 2015 Sebastian Schilbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LTMLabel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LTMLabel
+//
+//  Created by Sebastian Schilbe on 02.06.15.
+//  Copyright (c) 2015 Sebastian Schilbe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
